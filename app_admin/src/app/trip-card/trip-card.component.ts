3446fb0420e2b5751fdd7df31989cda7c23dd02d
William Leaver <william.leaver@snhu.edu>
2023-06-18 11:03:21 -0500

    Created Angular SPA and added the trip card/nav bar

---
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-card',
+  templateUrl: './trip-card.component.html',
+  styleUrls: ['./trip-card.component.css']
+})
+export class TripCardComponent implements OnInit {
+  @Input('trip') trip: any;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
