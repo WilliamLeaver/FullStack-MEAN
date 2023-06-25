c86c7a2768c7a8c73f8b9dfd72e91235e12bdfc6
William Leaver <william.leaver@snhu.edu>
2023-06-25 14:11:53 -0500

    Added security for backend/SPA

--- app_admin/src/app/trip-card/trip-card.component.ts
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { Router } from "@angular/router";
-import { Trip } from "../models/trip";
+import { Trip } from '../models/trip';
+import { AuthenticationService } from '../services/authentication';
 
 @Component({
   selector: 'app-trip-card',
@@ -8,16 +9,25 @@ import { Trip } from "../models/trip";
   styleUrls: ['./trip-card.component.css']
 })
 export class TripCardComponent implements OnInit {
-  @Input('trip') trip: any;
-  constructor(private router: Router) { }
+
+  @Input('trip') trip: Trip;
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) { }
 
   ngOnInit() {
-
   }
+
   private editTrip(trip: Trip): void {
     localStorage.removeItem("tripCode");
     localStorage.setItem("tripCode", trip.code);
-    this.router.navigate(["edit-trip"]);
+    this.router.navigate(['edit-trip']);
+  }
+
+  public isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
   }
 
 }
